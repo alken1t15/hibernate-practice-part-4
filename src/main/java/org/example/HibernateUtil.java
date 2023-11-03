7caafd64443912c5488c84e5884139af3ec435ae
Alexey <alken1t15@gmail.com>
2023-11-03 23:12:55 +0600

    Version 2

---
+++ src/main/java/org/example/HibernateUtil.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        try {
+            return new Configuration().configure(new File("src\\main\\resources\\hibernate.cfg.xml")).buildSessionFactory();
+        }catch (Throwable ex){
+            throw new RuntimeException();
+        }
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+    public static void shutdown(){
+        getSessionFactory().close();
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.hibernate.Session;
+
+import javax.management.Query;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.getTransaction().begin();
+
+     //   Query query = session.createQuery("select u from ");
+
+        session.close();
+        HibernateUtil.shutdown();
     }
 }
